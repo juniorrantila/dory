7d0975bafbe5c4e7b9d1ec7ed22a7aaef5e2e980
Junior Rantila <junior.rantila@gmail.com>
2023-02-02 19:08:05 +0100

    Main: Restart on failure

--- src/Main/Main.cpp
+++ src/Main/Main.cpp
@@ -1,12 +1,19 @@
 #include "Main.h"
-#include "Core/File.h"
+#include <Core/File.h>
+#include <Core/Print.h>
+#include <Core/System.h>
 
 int main(int argc, c_string argv[])
 {
-    auto result = Main::main(argc, argv);
-    if (result.is_error()) {
-        Core::File::stderr().writeln(result.error()).ignore();
-        return 1;
+    while(true) {
+        auto result = Main::main(argc, argv);
+        if (result.is_error()) {
+            Core::File::stderr().writeln("Error: "sv, result.error()).ignore();
+            dbgln("Restarting in 10 seconds\n"sv);
+            Core::System::sleep(10);
+            dbgln("Restarting"sv);
+            continue;
+        }
+        return result.value();
     }
-    return result.value();
 }
