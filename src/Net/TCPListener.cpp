fcc03fe5270ed13f4bad495c6a16564da2c5831b
Junior Rantila <junior.rantila@gmail.com>
2023-02-12 21:56:14 +0100

    Net: Make TCPListener steal port if used
    This makes it so that you don't have to wait between program crash
    and restarting.

--- src/Net/TCPListener.cpp
+++ src/Net/TCPListener.cpp
@@ -28,6 +28,9 @@ ErrorOr<TCPListener> TCPListener::create(u16 port,
     int socket = ::socket(res->ai_family, res->ai_socktype, res->ai_protocol);
     if (socket < 0)
         return Error::from_errno();
+    int yes = 1;
+    if (setsockopt(socket, SOL_SOCKET, SO_REUSEADDR, &yes, sizeof(yes)) < 0)
+        return Error::from_errno();
     if (bind(socket, res->ai_addr, res->ai_addrlen) < 0)
         return Error::from_errno();
     if (listen(socket, queued_connections) < 0) {
