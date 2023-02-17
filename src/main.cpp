1c6561d2de77fee970232f8df4c361301ccd31f1
Junior Rantila <junior.rantila@gmail.com>
2023-02-17 09:05:45 +0100

    src: Add very basic multi processing

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,7 @@
 #include <CLI/ArgumentParser.h>
 #include <Core/MappedFile.h>
 #include <Core/Print.h>
+#include <Core/System.h>
 #include <HTTP/Headers.h>
 #include <HTTP/Response.h>
 #include <Main/Main.h>
@@ -14,6 +15,20 @@
 #include <Web/File.h>
 #include <Web/FileRouter.h>
 #include <Web/MimeType.h>
+
+using Renderer = SmallCapture<ErrorOr<HTTP::Response>()>;
+using DynamicRouter = Ty::SmallMap<StringView, Renderer>;
+
+static ErrorOr<void> setup_zombie_reaper();
+
+struct Connection {
+    Net::TCPClientConnection& client;
+    Core::File& log;
+    Web::FileRouter& file_router;
+    DynamicRouter const& dynamic_router;
+    StringView static_folder_path;
+};
+static ErrorOr<void> handle_connection(Connection const& args);
 
 ErrorOr<int> Main::main(int argc, c_string argv[])
 {
@@ -71,9 +86,7 @@ ErrorOr<int> Main::main(int argc, c_string argv[])
     auto apple_touch_icon_path = TRY(StringBuffer::create_fill(static_folder_path, "/icons/apple-touch-icon.png"sv));
     TRY(file_router.add_route("/apple-touch-icon.png"sv, apple_touch_icon_path.view()));
 
-    using Renderer = SmallCapture<ErrorOr<HTTP::Response>()>;
-    using Router = Ty::SmallMap<StringView, Renderer>;
-    auto dynamic_router = Router();
+    auto dynamic_router = DynamicRouter();
 
     auto foo_json_path = TRY(StringBuffer::create_fill(static_folder_path, "/json/foo.json"sv));
     auto foo_json_file = TRY(Web::File::open(foo_json_path.view()));
@@ -101,78 +114,114 @@ ErrorOr<int> Main::main(int argc, c_string argv[])
 
     log.writeln("Serving on port: "sv, port).ignore();
 
+    TRY(setup_zombie_reaper());
     auto server = TRY(Net::TCPListener::create(port));
     while (true) {
         auto client = TRY(server.accept());
+
+        if (TRY(Core::System::fork()) > 0)
+            continue;
+        server.destroy().ignore();
+
         auto client_name = TRY(client.printable_address());
         log.writeln(client_name.view(), " connected"sv).ignore();
         Defer print_disconnect = [&] {
             log.writeln("dropped "sv, client_name.view()).ignore();
         };
-
-        auto raw_request = TRY(client.read());
-        log.writeln("\nrequest:\n"sv, raw_request.view(), "request end\n"sv).ignore();
-
-        if (raw_request.view().is_empty()) {
-            TRY(client.write(HTTP::Response {
-                .code = HTTP::ResponseCode::Continue,
-            }));
-            continue;
-        }
-
-        auto headers = TRY(HTTP::Headers::create_from(raw_request.view()));
-
-        log.writeln("parsed get: "sv, headers.get()).ignore();
-
-        auto maybe_get = TRY(headers.get());
-        if (!maybe_get.has_value()) {
-            TRY(client.write(HTTP::Response {
-                .body = "not found"sv,
-                .code = HTTP::ResponseCode::NotFound,
-            }));
-            continue;
-        }
-        auto get = maybe_get.release_value();
-
-        if (auto id = file_router.find(get.slug); id) {
-            TRY(file_router.reload_files_if_needed(log));
-            auto const& file = file_router[id.value()];
-            TRY(client.write(HTTP::Response {
-                .body = file.view(),
-                .charset = file.charset(),
-                .mime_type = file.mime_type(),
-                .code = HTTP::ResponseCode::Ok,
-            }));
-            continue;
-        }
-
-        if (auto id = dynamic_router.find(get.slug); id) {
-            auto route = dynamic_router[id.value()];
-            auto error_buffer = TRY(StringBuffer::create());
-            TRY(client.write(TRY(route().or_else([&](auto error) -> ErrorOr<HTTP::Response> {
-                error_buffer.clear();
-                TRY(error_buffer.write(error));
-                return HTTP::Response {
-                    .body = error_buffer.view(),
-                    .code = HTTP::ResponseCode::InternalServerError,
-                };
-            }).or_else([](auto) {
-                return HTTP::Response {
-                    .body = "Could not report error"sv,
-                    .code = HTTP::ResponseCode::InternalServerError,
-                };
-            }))));
-            continue;
-        }
-
-        auto not_found_path = TRY(StringBuffer::create_fill(static_folder_path, "/error/404.html"sv));
-        auto not_found_file = TRY(Web::File::open(not_found_path.view()));
-        TRY(client.write(HTTP::Response {
-            .body = not_found_file.view(),
-            .mime_type = not_found_file.mime_type(),
-            .code = HTTP::ResponseCode::NotFound,
-        }));
+        handle_connection({
+            .client = client,
+            .log = log,
+            .file_router = file_router,
+            .dynamic_router = dynamic_router,
+            .static_folder_path = static_folder_path,
+        }).or_else([&](auto error) {
+            log.writeln("Error: "sv, error).ignore();
+        });
+        return 0;
     }
 
     return 0;
 }
+
+static ErrorOr<void> setup_zombie_reaper()
+{
+    struct sigaction sa;
+    sa.sa_handler = [](auto) {
+        // waitpid() might overwrite errno, so we save and restore it:
+        int saved_errno = errno;
+        while(waitpid(-1, NULL, WNOHANG) > 0);
+        errno = saved_errno;
+    };
+    TRY(Core::System::sigemptyset(&sa.sa_mask));
+    sa.sa_flags = SA_RESTART;
+    TRY(Core::System::sigaction(SIGCHLD, &sa, nullptr));
+    return {};
+}
+
+static ErrorOr<void> handle_connection(Connection const& args)
+{
+    auto raw_request = TRY(args.client.read());
+    args.log.writeln("\nrequest:\n"sv, raw_request.view(), "request end\n"sv).ignore();
+
+    if (raw_request.view().is_empty()) {
+        TRY(args.client.write(HTTP::Response {
+            .extra_headers = "Accept: */*\r\n"sv,
+            .code = HTTP::ResponseCode::Continue,
+        }));
+        return {};
+    }
+
+    auto headers = TRY(HTTP::Headers::create_from(raw_request.view()));
+
+    args.log.writeln("parsed get: "sv, headers.get()).ignore();
+
+    auto maybe_get = TRY(headers.get());
+    if (!maybe_get.has_value()) {
+        TRY(args.client.write(HTTP::Response {
+            .body = "not found"sv,
+            .code = HTTP::ResponseCode::NotFound,
+        }));
+        return {};
+    }
+    auto get = maybe_get.release_value();
+
+    if (auto id = args.file_router.find(get.slug); id) {
+        TRY(args.file_router.reload_files_if_needed(args.log));
+        auto const& file = args.file_router[id.value()];
+        TRY(args.client.write(HTTP::Response {
+            .body = file.view(),
+            .charset = file.charset(),
+            .mime_type = file.mime_type(),
+            .code = HTTP::ResponseCode::Ok,
+        }));
+        return {};
+    }
+
+    if (auto id = args.dynamic_router.find(get.slug); id) {
+        auto route = args.dynamic_router[id.value()];
+        auto error_buffer = TRY(StringBuffer::create());
+        TRY(args.client.write(TRY(route().or_else([&](auto error) -> ErrorOr<HTTP::Response> {
+            error_buffer.clear();
+            TRY(error_buffer.write(error));
+            return HTTP::Response {
+                .body = error_buffer.view(),
+                .code = HTTP::ResponseCode::InternalServerError,
+            };
+        }).or_else([](auto) {
+            return HTTP::Response {
+                .body = "Could not report error"sv,
+                .code = HTTP::ResponseCode::InternalServerError,
+            };
+        }))));
+        return {};
+    }
+
+    auto not_found_path = TRY(StringBuffer::create_fill(args.static_folder_path, "/error/404.html"sv));
+    auto not_found_file = TRY(Web::File::open(not_found_path.view()));
+    TRY(args.client.write(HTTP::Response {
+        .body = not_found_file.view(),
+        .mime_type = not_found_file.mime_type(),
+        .code = HTTP::ResponseCode::NotFound,
+    }));
+    return {};
+};
