3efb204d54d1c6b4787d446855756549f76b93a1
Junior Rantila <junior.rantila@gmail.com>
2023-03-12 20:40:23 +0100

    Ty: Add Limits for u32 and u64

--- src/Ty/Limits.h
+++ src/Ty/Limits.h
@@ -15,4 +15,16 @@ struct Limits<u16> {
     static u16 min() { return 0; }
 };
 
+template <>
+struct Limits<u32> {
+    static u32 max() { return -1; }
+    static u32 min() { return 0; }
+};
+
+template <>
+struct Limits<u64> {
+    static u64 max() { return -1; }
+    static u64 min() { return 0; }
+};
+
 }
