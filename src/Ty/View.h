dc06c9e65684b761e427a3fe16d68684a89f56e2
Junior Rantila <junior.rantila@gmail.com>
2023-03-12 20:38:52 +0100

    Ty: VERIFY data integrity in View

--- src/Ty/View.h
+++ src/Ty/View.h
@@ -1,6 +1,7 @@
 #pragma once
 #include "Base.h"
 #include "Traits.h"
+#include "Verify.h"
 
 namespace Ty {
 
@@ -12,47 +13,53 @@ struct View {
     {
     }
 
-    T& operator[](usize index) { return m_data[index]; }
-
-    T const& operator[](usize index) const { return m_data[index]; }
-
-    constexpr T* begin() { return m_data; }
-    constexpr T* end() { return &m_data[m_size]; }
-
-    constexpr T const* begin() const { return m_data; }
-    constexpr T const* end() const { return &m_data[m_size]; }
-
-    constexpr usize size() const { return m_size; }
-    constexpr T* data() { return m_data; }
-    constexpr T const* data() const { return m_data; }
-
-private:
-    T* m_data;
-    usize m_size;
-};
-
-template <typename T>
-requires is_const<T>
-struct View<T> {
-    constexpr View(View<RemoveConst<T>> other)
-        : m_data(other.data())
-        , m_size(other.size())
+    T& operator[](usize index)
     {
+        VERIFY(index < size());
+        VERIFY(m_data);
+        return m_data[index];
     }
 
-    constexpr View(T* data, usize size)
-        : m_data(data)
-        , m_size(size)
+    T const& operator[](usize index) const 
     {
+        VERIFY(index < size());
+        VERIFY(m_data);
+        return m_data[index];
     }
 
-    T const& operator[](usize index) const { return m_data[index]; }
+    constexpr T* begin()
+    {
+        VERIFY(m_data);
+        return m_data;
+    }
+    constexpr T* end()
+    {
+        VERIFY(m_data);
+        return &m_data[m_size];
+    }
 
-    constexpr T const* begin() const { return m_data; }
-    constexpr T const* end() const { return &m_data[m_size]; }
+    constexpr T const* begin() const
+    {
+        VERIFY(m_data);
+        return m_data;
+    }
+    constexpr T const* end() const
+    { 
+        VERIFY(m_data);
+        return &m_data[m_size];
+    }
 
     constexpr usize size() const { return m_size; }
-    constexpr T const* data() const { return m_data; }
+    constexpr T* data()
+    {
+        VERIFY(m_data);
+        return m_data;
+    }
+    constexpr T const* data() const
+    {
+        VERIFY(m_data);
+        return m_data;
+    }
 
 private:
     T* m_data;
